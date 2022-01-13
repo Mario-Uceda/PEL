63b31b96ba8f30a9cb3b746cab105ed9dd160423
Mario Uceda <78346376+Mario-Uceda@users.noreply.github.com>
2022-01-13 13:47:18 +0100

    Delete Dlib1/build directory

--- Dlib1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mario/Documents/Universidad/2/PEL/Workspace/Act_Individual_Dlib
-# Build directory: C:/Users/Mario/Documents/Universidad/2/PEL/Workspace/Act_Individual_Dlib/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
