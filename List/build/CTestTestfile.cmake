456f88944c07e864d1cb702c9cf48a3e834bd025
Mario Uceda <78346376+Mario-Uceda@users.noreply.github.com>
2022-01-13 13:53:37 +0100

    Delete List/build directory

--- List/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mario/Documents/Universidad/2/PEL/Workspace/list
-# Build directory: C:/Users/Mario/Documents/Universidad/2/PEL/Workspace/list/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
