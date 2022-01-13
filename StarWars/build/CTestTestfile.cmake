9992ed89f604b65afaddee39b26cf9dfa1567a35
Mario Uceda <78346376+Mario-Uceda@users.noreply.github.com>
2022-01-13 13:54:39 +0100

    Delete StarWars/build directory

--- StarWars/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Mario/Documents/Universidad/2/PEL/Workspace/StarWars
-# Build directory: C:/Users/Mario/Documents/Universidad/2/PEL/Workspace/StarWars/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
